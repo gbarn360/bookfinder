6fbaca474ea8baf30c4f3f1f10db70f27cea8479
unknown <gbarn360@gmail.com>
2024-09-10 13:10:03 -0400

    updated uri for graphql on

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import { ReadListProvider } from './Utility/ReadListContext';
 import { ApolloClient, InMemoryCache,ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
+    uri: 'https://bookfinderapi.onrender.com/graphql',
     cache: new InMemoryCache(),
   });
   
